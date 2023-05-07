2b6e36502820e225d9c2b0b742632cad48ac582d
beOk91 <be_ok91@naver.com>
2023-05-07 22:11:49 +0900

    commit test

---
+++ src/bronze/Baekjoon1000.java
@@ -0,0 +1,11 @@
+package bronze;
+
+import java.util.Scanner;
+
+public class Baekjoon1000 {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int A = scan.nextInt(), B = scan.nextInt();
+		System.out.println(A + B);
+	}
+}
