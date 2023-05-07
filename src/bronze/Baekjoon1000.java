0520479f72b16a6ff7753615fb032cd54fd7e391
beOk91 <be_ok91@naver.com>
2023-05-07 22:19:31 +0900

    commit

--- src/bronze/Baekjoon1000.java
+++ src/bronze/Baekjoon1000.java
@@ -6,6 +6,6 @@ public class Baekjoon1000 {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		int A = scan.nextInt(), B = scan.nextInt();
-		System.out.println(A + B);
+		System.out.println(A + B); 
 	}
 }
